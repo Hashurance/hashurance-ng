74c637dcad1fe53ed27b4c85a17793e2716ff288
Valentin Fries <contact@fries.io>
2017-03-12 17:04:14 +0100

    Steps description

--- src/app/services/insurance/insurance.service.ts
+++ src/app/services/insurance/insurance.service.ts
@@ -1,14 +1,8 @@
 import {Injectable} from "@angular/core";
 const sha256 = require("sha256");
-import {SharedService} from '../../services/shared.service';
-import * as myGlobals from '../../globals';
 
 @Injectable()
 export class InsuranceService {
-
-  constructor(private ss: SharedService) {
-    ss.accounts = myGlobals.web3.eth.accounts;
-  }
 
   createContract(userId: string): Promise<string> {
     return new Promise((resolve, reject) => {
